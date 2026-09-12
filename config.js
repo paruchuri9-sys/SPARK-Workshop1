1e22bf0d92c30aa1216e89ed3a8795e2ae7b4657
paruchuri9-sys <paruchuri9@gmail.com>
2026-09-11 20:50:58 -0500

    Reduce polling load for workshop concurrency

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SPARK_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbydRLKhrr2McY3IeZ9T0Pe1lA9a3BNoL7Rz-Hd557_clOwEiwL1kFwcuqCu48tdOA6V8Q/exec",
-  POLL_MS: 2500,
+  POLL_MS: 5000,
   MODERATOR_KEY: "GoBears",
   GROUPS: ["Owl","Fox","Raven","Dolphin","Octopus"]
 };
