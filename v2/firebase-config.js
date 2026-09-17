b92294470083b9aacdbe1263516b917518c64097
paruchuri9-sys <paruchuri9@gmail.com>
2026-09-16 22:52:39 -0500

    Configure Firebase Realtime Database URL for SPARK v2

--- v2/firebase-config.js
+++ v2/firebase-config.js
@@ -1,8 +1,8 @@
 // Firebase web configuration for SPARK Scenario Player v2
-// Realtime Database URL will be added after the database is created.
 export const firebaseConfig = {
   apiKey: "AIzaSyD8j_fKK_83uI3hcmz_m-mKJXyT1dGLs-c",
   authDomain: "spark-scenario-player.firebaseapp.com",
+  databaseURL: "https://spark-scenario-player-default-rtdb.firebaseio.com/",
   projectId: "spark-scenario-player",
   storageBucket: "spark-scenario-player.firebasestorage.app",
   messagingSenderId: "242940185218",
