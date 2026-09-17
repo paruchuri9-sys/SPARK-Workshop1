3b34520e1dc866f7a510a902318bd19d557fb348
paruchuri9-sys <paruchuri9@gmail.com>
2026-09-16 22:52:12 -0500

    Add Firebase web app configuration for SPARK v2

---
+++ v2/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase web configuration for SPARK Scenario Player v2
+// Realtime Database URL will be added after the database is created.
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8j_fKK_83uI3hcmz_m-mKJXyT1dGLs-c",
+  authDomain: "spark-scenario-player.firebaseapp.com",
+  projectId: "spark-scenario-player",
+  storageBucket: "spark-scenario-player.firebasestorage.app",
+  messagingSenderId: "242940185218",
+  appId: "1:242940185218:web:49ce7bf483e28f6e06429b"
+};
