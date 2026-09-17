865e8967128cb6c9486ca6f8b1b9fdc1347d9142
paruchuri9-sys <paruchuri9@gmail.com>
2026-09-16 22:04:43 -0500

    Add Firebase config template for SPARK v2

---
+++ v2/firebase-config.example.js
@@ -0,0 +1,13 @@
+// Copy this file to firebase-config.js and replace the values with your Firebase web app config.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Only this Google account can operate moderator controls under the sample rules.
+export const moderatorEmail = "YOUR_MODERATOR_EMAIL@uca.edu";
